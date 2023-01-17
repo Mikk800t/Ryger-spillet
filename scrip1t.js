d220abda8fe23885aa95c90e0745162605f589d6
Mikk800t <77214656+Mikk800t@users.noreply.github.com>
2023-01-17 05:30:28 +0100

    Add files via upload

---
+++ scrip1t.js
@@ -0,0 +1,32 @@
+const randomButton = document.getElementById("random-button");
+const result = document.getElementById("result");
+const items = ["Gnist", "3x2 10", "Kongesug", "Håndbong", "5x15", "3x12", "4x12", "1 Sug", "2 Sug", "3 Sug", "4 Sug", "5 Sug", "6 Sug", "Bob Marley (Den Normale)", "Lyskryds", "Tænkeboks", "3x10", "Ninja", "Bob Marley (Den lange)"];
+const songs = ["song1.mp3", "song2.mp3", "song3.mp3", "song4.mp3"];
+const audio = new Audio();
+
+// Add the playMusic function
+function playMusic(index) {
+  audio.src = songs[index];
+  audio.play();
+}
+
+randomButton.addEventListener("click", function() {
+  let randomIndex = Math.floor(Math.random() * items.length);
+  result.textContent = items[randomIndex];
+  playMusic(randomIndex);
+});
+
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const textBox = document.getElementById("text-box");
+
+button1.addEventListener("click", function() {
+  textBox.style.display = "block";
+});
+button2.addEventListener("click", function() {
+  textBox.style.display = "block";
+});
+
+textBox.addEventListener("click", function() {
+  textBox.style.display = "none";
+});
